3dbf0223e22e055e0d5836a19775d0c3067cbb4b
Sumit <singhaman5536@gmail.com>
2022-08-02 12:01:06 +0530

    files are ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
