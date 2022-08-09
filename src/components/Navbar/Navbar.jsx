2fed8a308a650a3d67f01f12eb7cd9313c60eb01
Sumit <singhaman5536@gmail.com>
2022-08-09 17:21:19 +0530

    HOME && ABOUT

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
+import { images } from '../../constants';
+// second way to immport images from files..........++
+// import images from '../../constants/images';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+
+  // THIS WAY CALLING USESTATE CAN SAVE YOUR TIME TO CALL IT FROMM IMPORT ABOVE....
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return (
+    <>
+      <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+          <img src={images.gericht} alt='Navbar-logo' />
+        </div>
+
+        <ul className='app__navbar-links'>
+          <li className='p__opensans'><a href='#home'>Home</a></li>
+          <li className='p__opensans'><a href='#about'>About</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#awards'>Awards</a></li>
+          <li className='p__opensans'><a href='#contacts'>Contacts</a></li>
+        </ul>
+
+        <div className="app__navbar-login">
+          <a href="#login" className='p__opensans'>Login / Registration</a>
+          <div />
+          <a href="/" className='p__opensans'> Book Table</a>
+        </div>
+
+        <div className="app__navbar-smallscreen">
+          <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+          {toggleMenu && (
+            <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+
+              <ul className='app__navbar-smallscreen_links'>
+                <li className='p__opensans'><a href='#home' onClick={() => setToggleMenu(false)} >Home</a></li>
+                <li className='p__opensans'><a href='#about' onClick={() => setToggleMenu(false)} >About</a></li>
+                <li className='p__opensans'><a href='#menu' onClick={() => setToggleMenu(false)} >Menu</a></li>
+                <li className='p__opensans'><a href='#awards' onClick={() => setToggleMenu(false)} >Awards</a></li>
+                <li className='p__opensans'><a href='#contacts' onClick={() => setToggleMenu(false)} >Contacts</a></li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { images } from '../../constants';
 
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+  <>
+    <div className="app__aboutus app__bg flex__center section__padding" id='about'>
+
+      <div className="app__aboutus-overlay flex-center">
+        <img src={images.G} alt="G-overlay" />
+      </div>
+
+      <div className="app__aboutus-content flex__center">
+
+        <div className="app__aboutus-content_about">
+          <h1 className='headtext__cormorant'>About Us</h1>
+          <img src={images.spoon} alt="spoon-img" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Incidunt in deserunt consequuntur soluta reprehenderit deleniti ratione atque
+            inventore ipsum est?
+          </p>
+          <button className="custom__button">Know More</button>
+        </div>
+
+        <div className="app__aboutus-content_knife flex__center">
+          <img src={images.knife} alt="about_knife" />
+        </div>
+
+        <div className="app__aboutus-content_history">
+          <h1 className='headtext__cormorant'>Our History</h1>
+          <img src={images.spoon} alt="spoon-img" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Incidunt in deserunt consequuntur soluta reprehenderit deleniti ratione atque
+            inventore ipsum est?
+          </p>
+          <button className="custom__button">Know More</button>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default AboutUs;
