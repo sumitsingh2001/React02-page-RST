722d488e68f9adf2fe7d057ec53f940245a9ad29
Sumit <singhaman5536@gmail.com>
2022-08-10 14:05:53 +0530

    Menu-Section

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,7 +1,7 @@
 import images from './images';
 
 const wines = [
-  { title: 'Chapel Hill Shiraz', price: '$56', tag: 'AU | Bottle' },
+  { title: 'Chapel Hill Shiraz', price: '$56', tags: 'AU | Bottle' },
   { title: 'Catena Malbee', price: '$59', tags: 'AU | Bottle' },
   { title: 'La Vieillw Rose', price: '$44', tags: 'FR | 750 ml' },
   { title: 'Rhino Pale Ale', price: '$31', tags: 'CA | 750 ml' },
